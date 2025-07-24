2273071efed9739a5fdabf57bca4bc2b067d19b6
bipin69 <126150132+bipin69@users.noreply.github.com>
2025-07-24 14:00:45 +0545

    admin

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+const Navbar = ({setToken}) => {
+  return (
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <p className="font-display text-2xl text-primary">
+        Fresh Meat Admin Dashboard
+      </p>
+      <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
